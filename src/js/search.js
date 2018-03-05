41a3b3d6e019a2cbf5149954e228f1c31887da66
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-03-05 10:08:49 -0300

    add weather icons for each city

--- src/js/search.js
+++ src/js/search.js
@@ -19,17 +19,20 @@ function getWeather(e) {
     .then(response => response.json())
     .then(data => {
       document.querySelector("#icon-div").innerHTML = `
-        <i class="icon wi wi-day-sunny"></i>
+        <i class="icon ${selectIcon(data.weather[0].icon)}"/>
         <h2 class="active" href="#" id="fahrenheit">${Math.round(
           (data.main.temp - 273.15) * 1.8 + 32
         )}°F | ${Math.round(data.main.temp - 273.15)}°C</h2>
         <br>
-            <h3>Wind: ${data.wind.speed} mph</h3>
-            <h3>Humidity: ${data.main.humidity}%</h3>
+            <h3>
+                Wind: ${data.wind.speed} mph
+                <br>
+                Humidity: ${data.main.humidity}%
+            </h3>
         <br>
       `;
       document.querySelector("#details-div").innerHTML = `
-          <h2 class="details" id="city">${data.name}, ${data.sys.country}</h2>
+          <h2 >${data.name}, ${data.sys.country}</h2>
           <br>
           <section>
             <h3>${dayName[now.getDay()]} ${now.getHours()}:${getMinute(
@@ -61,4 +64,48 @@ function clearInput() {
 function getMinute(time) {
   let min = ("0" + time.getMinutes()).slice(-2);
   return min;
+}
+
+function selectIcon(code) {
+  switch (code) {
+    case "01d":
+    case "01n":
+      return `wi wi-day-sunny`;
+      break;
+    case "02d":
+    case "02n":
+      return `wi wi-night-cloudy`;
+      break;
+    case "03d":
+    case "03n":
+      return `wi wi-cloud`;
+      break;
+    case "04d":
+    case "04n":
+      return `wi wi-cloudy`;
+      break;
+    case "09d":
+    case "09n":
+      return `wi wi-showers`;
+      break;
+    case "10d":
+    case "10n":
+      return `wi wi-rain`;
+      break;
+    case "11d":
+    case "11n":
+      return `wi wi-thunderstorm`;
+      break;
+    case "13d":
+    case "13n":
+      return `wi wi-snow-wind`;
+      break;
+    case "50d":
+    case "50n":
+      return `wi wi-fog`;
+      break;
+    default:
+      return `wi wi-day-sunny`;
+      break;
+  }
 }
