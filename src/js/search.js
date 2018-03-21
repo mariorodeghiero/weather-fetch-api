0ec64e0685e8a55c748c9629b589e7c5f0bfe1e0
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-03-20 22:01:25 -0300

    upadte forecast v0.0.2

--- src/js/search.js
+++ src/js/search.js
@@ -112,35 +112,34 @@ function getWeatherWeek(city) {
         arrDate.push(date);
 
         document.querySelector("#outPutForecast").innerHTML = `
-         <div class="box">
+        <div class="box">
           <h3>Mim</h3>
-          <br>
           <h3>Max</h3>
         </div>
         <div class="box" id="day-1">
           <h4>${arrDate[0]}</h4>
-          <p>${fahrenheit(data.list[1].main.temp_max)}</p>
-          <p>${fahrenheit(data.list[1].main.temp_min)}</p>
+          <p>${fahrenheit(data.list[1].main.temp_max)}°F</p>
+          <p>${fahrenheit(data.list[1].main.temp_min)}°F</p>
         </div>
         <div class="box" id="day-2">
           <h4>${arrDate[1]}</h4>
-          <p>${fahrenheit(data.list[2].main.temp_max)}</p>
-          <p>${fahrenheit(data.list[2].main.temp_min)}</p>
+          <p>${fahrenheit(data.list[2].main.temp_max)}°F</p>
+          <p>${fahrenheit(data.list[2].main.temp_min)}°F</p>
         </div>
         <div class="box" id="day-3">
           <h4>${arrDate[2]}</h4>
-          <p>${fahrenheit(data.list[10].main.temp_max)}</p>
-          <p>${fahrenheit(data.list[10].main.temp_min)}</p>
+          <p>${fahrenheit(data.list[10].main.temp_max)}°F</p>
+          <p>${fahrenheit(data.list[10].main.temp_min)}°F</p>
         </div>
         <div class="box" id="day-4">
           <h4>${arrDate[3]}</h4>
-          <p>${fahrenheit(data.list[18].main.temp_max)}</p>
-          <p>${fahrenheit(data.list[18].main.temp_min)}</p>
+          <p>${fahrenheit(data.list[18].main.temp_max)}°F</p>
+          <p>${fahrenheit(data.list[18].main.temp_min)}°F</p>
         </div>
         <div class="box-end" id="day-5">
           <h4>${arrDate[4]}</h4>
-          <p>${fahrenheit(data.list[26].main.temp_max)}</p>
-          <p>${fahrenheit(data.list[26].main.temp_min)}</p>
+          <p>${fahrenheit(data.list[26].main.temp_max)}°F</p>
+          <p>${fahrenheit(data.list[26].main.temp_min)}°F</p>
         </div>
         `;
       });
